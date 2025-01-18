59281bbaf389fe7a1da986a33c055a04dbbde401
Paulo César Conduru <133069658+pcconduru@users.noreply.github.com>
2025-01-18 00:31:26 -0300

    Atualização de Interface
    Adicionado todos os componentes de tela responsáveis pela interface nesta aplicação

--- app/src/main/java/com/example/calculadoraimc/ResultadoActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/ResultadoActivity.kt
@@ -1,35 +1,22 @@
 package com.example.calculadoraimc
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    lateinit var btnCalcular: Button
-
+class ResultadoActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_resultado)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        btnCalcular = findViewById(R.id.btn_calcular)
-        btnCalcular.setOnClickListener {
-            val intent = Intent(
-                this,
-                ResultadoActivity::class.java
-            )
-            startActivity( intent )
         }
     }
 }
